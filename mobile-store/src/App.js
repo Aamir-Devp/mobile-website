8583711e3ef8bf460c62e0f430044ff5c90ed948
Syed Aamirsohail <syedaamirsohail@Syeds-Mac-mini.local>
2024-10-31 23:55:17 +0530

    Add ons

---
+++ mobile-store/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.js"
+import { BrowserRouter as Router} from "react-router-dom"
+import Slider from "./components/Slider.js"
+function App() {
+  return (
+    <Router>
+      <PreNavbar/>
+      <Navbar/>
+      <Slider/>
+    </Router>
+  );
+}
+
+export default App;
