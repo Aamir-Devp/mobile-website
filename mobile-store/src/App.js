ecff549ee1e1d32acd37ac5fe7f754ce565a9d92
Syed Aamirsohail <syedaamirsohail@Syeds-Mac-mini.local>
2024-11-01 23:47:46 +0530

    New Components

--- mobile-store/src/App.js
+++ mobile-store/src/App.js
@@ -4,12 +4,15 @@ import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
 import { BrowserRouter as Router} from "react-router-dom"
 import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from"./components/Offers.js"
 function App() {
   return (
     <Router>
       <PreNavbar/>
       <Navbar/>
-      <Slider/>
+      <Slider start={data.banner.start}/>
+      <Offers offers={data.offer}/>
     </Router>
   );
 }
