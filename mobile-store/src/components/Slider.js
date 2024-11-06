6af9ccd3513b025e435b429a0669570d1ac07790
Syed Aamirsohail <syedaamirsohail@Syeds-Mac-mini.local>
2024-11-06 18:10:18 +0530

    bug fixes

--- mobile-store/src/components/Slider.js
+++ mobile-store/src/components/Slider.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import CarouselCaption from 'react-bootstrap/esm/CarouselCaption';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Slider = ({start}) => {
